4babbbef01d20a4afcd0f20d74632ccca46586c8
Jeongho Noh <shwjdgh34@gmail.com>
2019-03-22 22:47:31 +0900

    divided link to web and mobile

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const request = require("request")
 const bodyParser = require('body-parser')
 const cookie = require("cookie")
 
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,7 +14,7 @@ app.get('/', (req, res) => {
     let html = template.HTML();
     res.send(html);
 })
-app.get('/kakaopay_process',(req,res) =>{
+app.get('/kakaopay_process_web',(req,res) =>{
     let bodycontent = {
         "cid":"TC0ONETIME" ,
         "partner_order_id":"partner_order_id" ,
@@ -45,6 +44,38 @@ app.get('/kakaopay_process',(req,res) =>{
         res.cookie('tid', body.tid);
         //res.cookie('name', 'tobi', { domain: '.example.com', path: '/admin', secure: true }); 
         res.redirect(body.next_redirect_pc_url);
+    });
+})
+app.get('/kakaopay_process_mobile',(req,res) =>{
+    let bodycontent = {
+        "cid":"TC0ONETIME" ,
+        "partner_order_id":"partner_order_id" ,
+        "partner_user_id":"partner_user_id" ,
+        "item_name":"초코파이" ,
+        "quantity":"1" ,
+        "total_amount":"2200" ,
+        "vat_amount":"200" ,
+        "tax_free_amount":"0" ,
+        "approval_url":"http://www.xn--en1bo2f3ybe01a6jg9xi.com:3000/help" ,
+        "fail_url":"http://www.xn--en1bo2f3ybe01a6jg9xi.com:3000/help" ,
+        "cancel_url":"http://www.xn--en1bo2f3ybe01a6jg9xi.com:3000/help  "
+        // "approval_url":"http://localhost:3000/help" ,
+        // "fail_url":"http://localhost:3000/help" ,
+        // "cancel_url":"http://localhost:3000/help"
+    };
+    request.post({
+        headers: {'content-type' : 'application/x-www-form-urlencoded',
+                    "Authorization": "KakaoAK 1a8e9a0a3c6926925c24bc9d5bc51bb4",
+                    },
+        url:     "https://kapi.kakao.com/v1/payment/ready",
+        form: bodycontent,
+        json: true
+    }, function(error, response, body){
+        console.log(body); 
+        //res.set 
+        res.cookie('tid', body.tid);
+        //res.cookie('name', 'tobi', { domain: '.example.com', path: '/admin', secure: true }); 
+        res.redirect(body.next_redirect_mobile_url);
     });
 })
 
